0bf0d797b4e97acc11ab29d9d6d533526f6ef9c0
Samarth Dave <samarthdave@users.noreply.github.com>
2019-03-14 02:04:11 -0500

    Jumbotron and homepage
    Use library and flexbox to customize homepage

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Background from "./media/background-min.png";
+import { Pane, Text, Button } from "evergreen-ui";
+
 import "./styles.css";
 
 function App() {
+  const appStyle = {
+    backgroundImage: `url(${Background})`
+  };
   return (
-    <div className="App">
-      <div className="jumbotron">
+    <div className="App" style={appStyle}>
+      <Pane
+        className="jumbotron"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        border="default"
+      >
         <h1 className="jumbotron-title">SearchSpace &#128640;</h1>
         <h2 className="jumbotron-subtitle">Explore beyond and within</h2>
-      </div>
+        <Button
+          appearance="primary"
+          intent="success"
+          id="get-started-btn"
+          alignItems="center"
+          justifyContent="center"
+          height={40}
+        >
+          Get started
+        </Button>
+      </Pane>
     </div>
   );
 }
